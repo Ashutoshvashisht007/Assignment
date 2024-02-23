89927d7947dad2172e0ab208d7a2c01b4c0ea6ef
Ashutosh <ashuhina.123@gmail.com>
2024-02-23 13:18:10 +0530

    Navbar Done

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbarSearch">
+                <img src="/src/assets/images/IMAGE.png" alt="" />
+                <input type="text" />
+            </div>
+            <div className="navbarRight">
+                <span>Categories</span>
+                <span>Website Builders</span>
+                <span>Today's deals</span>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Navbar
